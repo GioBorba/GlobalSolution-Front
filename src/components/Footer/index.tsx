41ff82e6e4b92a9aaf2fc88fa1011fdd0832a206
Eduardo <eduardorshiraga@gmail.com>
2024-06-07 17:31:35 -0300

    alteração info no span

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <footer className="bg-gradient-to-r from-[#00a8e8] to-[#0077b6] text-white py-5 flex justify-center items-center">
             <div className="text-sm flex flex-col sm:flex-row items-center gap-2">
             <span>Giovani Shiraishi Borba (RM553724)</span>
-            <span className="sm:before:content-['|'] sm:before:mx-2">Eduardo Rodrigues Shiraga (RM123456)</span>
+            <span className="sm:before:content-['|'] sm:before:mx-2">Eduardo Rodrigues Shiraga (RM553705)</span>
             </div>
         </footer>
         </>
